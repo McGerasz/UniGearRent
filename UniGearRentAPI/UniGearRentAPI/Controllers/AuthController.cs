9128e75cbe5ffe1457fedefafbff9a944feb8cbd
McGerasz <mcgerasz@gmail.com>
2023-11-29 14:16:51 +0100

    feat: add logging to AuthController

--- UniGearRentAPI/UniGearRentAPI/Controllers/AuthController.cs
+++ UniGearRentAPI/UniGearRentAPI/Controllers/AuthController.cs
@@ -13,27 +13,32 @@ public class AuthController : ControllerBase
 {
     private readonly IAuthService _authService;
     private readonly UniGearRentAPIDbContext _dbContext;
+    private readonly ILogger<AuthController> _logger;
 
-    public AuthController(IAuthService authService, UniGearRentAPIDbContext dbContext)
+    public AuthController(IAuthService authService, UniGearRentAPIDbContext dbContext, ILogger<AuthController> logger)
     {
         _authService = authService;
         _dbContext = dbContext;
+        _logger = logger;
     }
     [HttpPost("RegisterUser")]
     public async Task<ActionResult<RegistrationResponse>> RegisterUser(UserRegistrationRequest request)
     {
+        _logger.LogInformation("Beginning operation");
+        _logger.LogInformation("Validating model state...");
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
-
+        _logger.LogInformation("Registering new user...");
         var result = await _authService.RegisterAsync(request.Email, request.Username, request.PhoneNumber,
             request.Password, "User");
-
+        
         if (!result.Success)
         {
             AddErrors(result);
             return BadRequest(ModelState);
         }
-        Console.WriteLine(result.UserId);
+        _logger.LogInformation("Registration successful");
+        _logger.LogInformation("Updating database with new UserDetails...");
         await _dbContext.UsersDetails.AddAsync(new UserDetails
         {
             FirstName = request.FirstName,
@@ -41,14 +46,17 @@ public class AuthController : ControllerBase
             PosterId = result.UserId
         });
         await _dbContext.SaveChangesAsync();
+        _logger.LogInformation("Operation successful");
         return CreatedAtAction(nameof(RegisterUser), new RegistrationResponse(result.Email, result.UserName, result.PhoneNumber));
     }
     [HttpPost("RegisterLessor")]
     public async Task<ActionResult<RegistrationResponse>> RegisterLessor(LessorRegistrationRequest request)
     {
+        _logger.LogInformation("Beginning operation");
+        _logger.LogInformation("Validating model state...");
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
-
+        _logger.LogInformation("Registering new user...");
         var result = await _authService.RegisterAsync(request.Email, request.Username, request.PhoneNumber,
             request.Password, "Lessor");
 
@@ -57,30 +65,35 @@ public class AuthController : ControllerBase
             AddErrors(result);
             return BadRequest(ModelState);
         }
-
+        _logger.LogInformation("Registration successful");
+        _logger.LogInformation("Updating database with new LessorDetails...");
         await _dbContext.LessorsDetails.AddAsync(new LessorDetails
         {
             Name = request.Name,
             PosterId = result.UserId
         });
         await _dbContext.SaveChangesAsync();
+        _logger.LogInformation("Operation successful");
         return CreatedAtAction(nameof(RegisterUser), new RegistrationResponse(result.Email, result.UserName, result.PhoneNumber));
     }
     
     [HttpPost("Login")]
     public async Task<ActionResult<AuthResponse>> Authenticate([FromBody] AuthRequest request)
     {
+        _logger.LogInformation("Beginning operation");
+        _logger.LogInformation("Validating model state...");
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
-
+        _logger.LogInformation("Attempting to login...");
         var result = await _authService.LoginAsync(request.Email, request.Password);
 
         if (!result.Success)
         {
+            _logger.LogError("Login attempt unsuccessful");
             AddErrors(result);
             return BadRequest(ModelState);
         }
-
+        _logger.LogInformation("Operation successful");
         return Ok(new AuthResponse(result.Email, result.UserName, result.PhoneNumber,result.Token));
     }
     
