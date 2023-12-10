27a0c536d28ed90faf12641390efd3f61979ec24
McGerasz <mcgerasz@gmail.com>
2023-12-10 10:03:26 +0100

    feat: add migration for column typo fix

--- UniGearRentAPI/UniGearRentAPI/Migrations/20231210085345_fixDescriptionTypo.Designer.cs
+++ UniGearRentAPI/UniGearRentAPI/Migrations/20231210085345_fixDescriptionTypo.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UniGearRentAPI.DatabaseServices;
@@ -11,9 +12,11 @@ using UniGearRentAPI.DatabaseServices;
 namespace UniGearRentAPI.Migrations
 {
     [DbContext(typeof(UniGearRentAPIDbContext))]
-    partial class UniGearRentAPIDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231210085345_fixDescriptionTypo")]
+    partial class fixDescriptionTypo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
