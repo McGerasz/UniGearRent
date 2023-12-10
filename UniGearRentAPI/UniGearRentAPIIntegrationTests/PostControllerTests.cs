c45f3b4b589e34a1f23fec43d09a4fc2bba51fcd
McGerasz <mcgerasz@gmail.com>
2023-12-10 11:21:04 +0100

    test: add setup for PostController

--- UniGearRentAPI/UniGearRentAPIIntegrationTests/PostControllerTests.cs
+++ UniGearRentAPI/UniGearRentAPIIntegrationTests/PostControllerTests.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Identity;
+using UniGearRentAPI.Models;
+
 namespace UniGearRentAPIIntegrationTests;
 
 public class PostControllerTests
@@ -14,6 +17,52 @@ public class PostControllerTests
         Environment.SetEnvironmentVariable("ASPNETCORE_ADMINEMAIL", "admin@admin.com");
         Environment.SetEnvironmentVariable("ASPNETCORE_ADMINPASSWORD", "Adminpassword123");
         _factory = new CustomWebApplicationFactory();
+        _factory._testUniGearRentAPIDbContext.Users.Add(new IdentityUser
+        {
+            AccessFailedCount = 0,
+            EmailConfirmed = true,
+            Id = "TESTID1",
+            LockoutEnabled = false,
+            UserName = "TestUser1"
+        });
+        _factory._testUniGearRentAPIDbContext.Users.Add(new IdentityUser
+        {
+            AccessFailedCount = 0,
+            EmailConfirmed = true,
+            Id = "TESTID2",
+            LockoutEnabled = false,
+            UserName = "TestUser2"
+        });
+        _factory._carRepository.Create(new CarPost
+        {
+            Id = 1,
+            CanDeliverToYou = true,
+            Description = "",
+            Location = "Budapest",
+            Name = "TESTCAR1",
+            NumberOfSeats = 4,
+            PosterId = "TESTID1"
+        });
+        _factory._trailerRepository.Create(new TrailerPost
+        {
+            Id = 2,
+            Description = "",
+            Location = "Miskolc",
+            Name = "TESTTRAILER1",
+            PosterId = "TESTID1",
+            Width = 100,
+            Length = 100
+        });
+        _factory._carRepository.Create(new CarPost
+        {
+            Id = 3,
+            CanDeliverToYou = true,
+            Description = "",
+            Location = "Budapest",
+            Name = "TESTCAR2",
+            NumberOfSeats = 4,
+            PosterId = "TESTID2"
+        });
         _client = _factory.CreateClient();
     }
     [OneTimeTearDown]
@@ -26,12 +75,12 @@ public class PostControllerTests
     [Test]
     public async Task GetByUserTest()
     {
-        
+        Assert.Pass();
     }
 
     [Test]
     public async Task GetByLocationTest()
     {
-        
+        Assert.Pass();
     }
 }
