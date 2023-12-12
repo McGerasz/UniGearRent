c4c206d118373eb60f28ab16510ca39e73cde640
McGerasz <mcgerasz@gmail.com>
2023-12-12 11:22:03 +0100

    feat: add Navbar component

---
+++ unigearrent-frontend/src/Components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import BootstrapNavbar from 'react-bootstrap/Navbar';
+import { Container, Nav, NavbarBrand } from 'react-bootstrap';
+import {Link, NavLink} from 'react-router-dom';
+
+const Navbar: React.FC = () => {
+    return(
+    <BootstrapNavbar style={{backgroundColor: "	#d9b99b"}} data-bs-theme="light">
+        <Container>
+            <Nav className='me-auto'>
+                <NavbarBrand>
+                <Link className="link-dark link-offset-2 link-underline-opacity-0 link-underline-opacity-0-hover" to="/">
+                        UniGearRent
+                </Link>
+                </NavbarBrand>
+            </Nav>
+            <Nav>
+            <Link className="link-dark link-offset-2 link-underline-opacity-0 link-underline-opacity-0-hover" to="/">
+                        Log in
+                </Link>
+            </Nav>
+        </Container>
+    </BootstrapNavbar>
+    )
+}
+export default Navbar;
