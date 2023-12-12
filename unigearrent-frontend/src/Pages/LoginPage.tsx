053165bd25a000cb3410fc76e90e190210d27fbc
McGerasz <mcgerasz@gmail.com>
2023-12-12 14:51:41 +0100

    feat: add login page

--- unigearrent-frontend/src/Pages/LoginPage.tsx
+++ unigearrent-frontend/src/Pages/LoginPage.tsx
@@ -1,4 +1,9 @@
+import { Container } from "react-bootstrap"
+import LoginComponent from "../Components/LoginComponent"
+
 const LoginPage: React.FC = () => {
-    return(<></>)
+    return(<Container className="justify-content-md-center">
+        <LoginComponent />
+    </Container>)
 }
 export default LoginPage
