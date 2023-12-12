7992df487d906b745db4b809bbd7795e18f88944
McGerasz <mcgerasz@gmail.com>
2023-12-12 11:21:22 +0100

    feat: add routing to App.tsx

--- unigearrent-frontend/src/App.tsx
+++ unigearrent-frontend/src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, createBrowserRouter, RouterProvider, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Layout from './Pages/Layout';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      errorElement: <Layout/>
+    }
+  ]);
   return (
-    <></>
+    <>
+    <RouterProvider router={router} />
+    </>
   );
 }
 
