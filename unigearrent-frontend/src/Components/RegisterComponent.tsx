9fc7fb871870a334e8cb3a23815bdcb2ff3606a8
McGerasz <mcgerasz@gmail.com>
2023-12-13 14:58:43 +0100

    fix: type notation of state

--- unigearrent-frontend/src/Components/RegisterComponent.tsx
+++ unigearrent-frontend/src/Components/RegisterComponent.tsx
@@ -9,7 +9,7 @@ import UserRegistration from './UserRegistration'
 import LessorRegistration from './LessorRegistration';
 const RegisterComponent: React.FC = () =>{
     const [registerType, setRegisterType] = useState<RegistrationType>()
-    const [typeSelected, setTypeSelected] = useState(false)
+    const [typeSelected, setTypeSelected] = useState<boolean>(false)
     return(
         <>{typeSelected ? (registerType === RegistrationType.User.valueOf() ? (<UserRegistration typeSelectedSetter={setTypeSelected}/>) : (<LessorRegistration typeSelectedSetter={setTypeSelected}/>)) : <RegisterTypeComponent registerTypeSetter={setRegisterType} typeSelectedSetter={setTypeSelected} />}</>
       )
