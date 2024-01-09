aa4c4807c87570a7e8ca8fb0c095c5d50389ce42
McGerasz <mcgerasz@gmail.com>
2024-01-09 19:30:58 +0100

    chore: remove unused code

--- unigearrent-frontend/src/Pages/RegisterPage.tsx
+++ unigearrent-frontend/src/Pages/RegisterPage.tsx
@@ -3,7 +3,6 @@ import RegisterComponent from "../Components/RegisterComponent"
 import { useState } from "react"
 
 const RegisterPage: React.FC = () => {
-    const [typeSelected, setTypeSelected] = useState(false)
     return(<Container className="justify-content-md-center">
         <RegisterComponent />
     </Container>)
