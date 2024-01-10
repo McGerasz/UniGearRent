bc99be2103c70b54de9f05c47e1bfa70dcc2e31e
McGerasz <mcgerasz@gmail.com>
2024-01-10 14:55:27 +0100

    feat: add PostCardsComponent

---
+++ unigearrent-frontend/src/Components/PostCardsComponent.tsx
@@ -0,0 +1,25 @@
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { PostCardData } from "../Models/PostCardData";
+import { useNavigate } from "react-router-dom";
+import { ReactNode } from "react";
+
+const PostCardsComponent: React.FC<{searchData: Array<PostCardData>}> = (props) => {
+    const ClickHandler: (id: number) => void = (id) => {
+        useNavigate()(`/Post/${id}`);
+    }
+    return(
+        <Row sm={1} md={2} lg={4}>{props.searchData.map((element: PostCardData) => {
+            console.log(element)
+            return <Col>
+                <Card onClick={() => ClickHandler(element.Id)} className="h-100">
+                <Card.Body>
+                    <Card.Title style={{textAlign:"center"}}>{element.Name}</Card.Title>
+                    <Card.Text  style={{textAlign:"center"}}>{element.Description}</Card.Text>
+                    <Card.Text  style={{textAlign:"center"}}>{element.Location}</Card.Text>    
+                </Card.Body>       
+            </Card>
+            </Col>
+        })}</Row>
+    )
+}
+export default PostCardsComponent;
--- unigearrent-frontend/src/Pages/SearchPage.tsx
+++ unigearrent-frontend/src/Pages/SearchPage.tsx
@@ -1,10 +1,15 @@
 import { Container } from "react-bootstrap"
 import { useState } from "react"
 import PostSearchComponent from "../Components/PostSearchComponent"
+import { PostCardData } from "../Models/PostCardData";
+import PostCardsComponent from "../Components/PostCardsComponent";
 
 const SearchPage: React.FC = () => {
-    return(<Container className="justify-content-md-center w-75 mt-5">
-        <PostSearchComponent />
+    const [searchData, setSearchData] = useState<Array<PostCardData>>(new Array<PostCardData>());
+    return(
+    <Container className="justify-content-md-center w-75 mt-5">
+        <PostSearchComponent searchDataSetter={setSearchData}/>
+        {searchData.length > 0 ? <PostCardsComponent searchData={searchData}/> : <>bar</>}
     </Container>)
 }
 export default SearchPage
