3d950d12df98daee8032e5974af31bea893f0449
McGerasz <mcgerasz@gmail.com>
2024-01-23 10:27:54 +0100

    feat: add styling to PostCards

--- unigearrent-frontend/src/Components/PostCardsComponent.tsx
+++ unigearrent-frontend/src/Components/PostCardsComponent.tsx
@@ -14,7 +14,7 @@ const PostCardsComponent: React.FC<{searchData: Array<PostCardData>}> = (props) 
                 <Card onClick={() => ClickHandler(element.Id)} className="h-100">
                 <Card.Body>
                     <Card.Title style={{textAlign:"center"}}>{element.Name}</Card.Title>
-                    <Card.Text  style={{textAlign:"center"}}>{element.Description}</Card.Text>
+                    <Card.Text  style={{textAlign:"center", lineHeight:"1.2em", height:"4.8em", overflow:"hidden", WebkitMaskImage:"linear-gradient(to bottom, black 50%, transparent 100%)", maskImage:"linear-gradient(to bottom, black 50%, transparent 100%)"}}>{element.Description}</Card.Text>
                     <Card.Text  style={{textAlign:"center"}}>{element.Location}</Card.Text>    
                 </Card.Body>       
             </Card>
