31afdca3e6b85926354f539024220e02f5a43173
McGerasz <mcgerasz@gmail.com>
2024-01-24 11:11:42 +0100

    feat: add functionality to delete button

--- unigearrent-frontend/src/Components/PostElement.tsx
+++ unigearrent-frontend/src/Components/PostElement.tsx
@@ -1,12 +1,30 @@
 import { JsxElement } from "typescript";
 import { PostType } from "../Models/PostType";
 import { Button, Col, Container, Row } from "react-bootstrap";
+import BackendURL from "../Utils/BackendURL";
+import { useNavigate } from "react-router-dom";
+import { useUserProfile } from "../Utils/UserProfileContextProvider";
 
 const PostElement: React.FC<{PostData: any, PostDataType: PostType}> = (props) => {
+    const profile = useUserProfile().userProfile;
+    const navigate = useNavigate();
     const priceVisualizer: () => JSX.Element= () => {
         const priceArr = [props.PostData["hourlyPrice"],props.PostData["dailyPrice"],props.PostData["weeklyPrice"], props.PostData["securityDeposit"]];
         if(!priceArr.some(element => element !== null)) return <></>
         return <Row className="mb-5 text-center">{priceArr[0] ? <Col>Hourly price: {priceArr[0]} HUF</Col> : <></>}{priceArr[1] ? <Col>Daily price: {priceArr[1]} HUF</Col> : <></>}{priceArr[2] ? <Col>Weekly price: {priceArr[2]} HUF</Col> : <></>}{priceArr[3] ? <Col>Security deposit: {priceArr[3]} HUF</Col> : <></>}</Row>
+    }
+    const deleteHandler: () => void = async () => {
+        if(window.confirm("Are you sure you want to delete this post?") === true){
+            await fetch(BackendURL + (props.PostDataType === PostType.Car ? "Car/" : "Trailer/") + props.PostData["id"], {
+                method: "DELETE",
+                mode: "cors",
+                headers: {
+                    "Authorization": "Bearer " + profile?.Token
+                }});
+            alert("Post successfully deleted")
+            navigate("/myposts")
+        }
+        return;
     }
     return(
     <Container>
@@ -30,7 +48,7 @@ const PostElement: React.FC<{PostData: any, PostDataType: PostType}> = (props) =
             <Button>Edit post</Button>
         </Col>
         <Col className="d-flex justify-content-center">
-            <Button variant="danger">Delete post</Button>
+            <Button variant="danger" onClick={deleteHandler}>Delete post</Button>
         </Col>
     </Row>
     </Container>)
