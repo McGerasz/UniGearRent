94bf27b4c30360daf944bb368f48e43a95dce4b1
McGerasz <mcgerasz@gmail.com>
2024-02-21 14:40:29 +0100

    feat: add new property to LessorPageDataResponse

--- UniGearRentAPI/UniGearRentAPI/Contracts/LessorPageDataResponse.cs
+++ UniGearRentAPI/UniGearRentAPI/Contracts/LessorPageDataResponse.cs
@@ -1,3 +1,5 @@
+using UniGearRentAPI.Models;
+
 namespace UniGearRentAPI.Contracts;
 
 public class LessorPageDataResponse
@@ -5,4 +7,5 @@ public class LessorPageDataResponse
     public string Name { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
+    public ICollection<Post> Posts { get; set; }
 }
