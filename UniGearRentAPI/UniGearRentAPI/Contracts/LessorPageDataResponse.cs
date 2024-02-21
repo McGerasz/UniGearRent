0f5e4bce6589f2d0bdb58d69b02c029f86f845c7
McGerasz <mcgerasz@gmail.com>
2024-02-21 09:52:39 +0100

    feat: add LessorPageDataResponse contract

---
+++ UniGearRentAPI/UniGearRentAPI/Contracts/LessorPageDataResponse.cs
@@ -0,0 +1,8 @@
+namespace UniGearRentAPI.Contracts;
+
+public class LessorPageDataResponse
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
