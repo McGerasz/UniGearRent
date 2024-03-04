5b123993bc56a787a94461188680fe875f63b81c
McGerasz <mcgerasz@gmail.com>
2024-03-04 13:10:56 +0100

    feat: update Profile model to include account id

--- unigearrent-frontend/src/Models/Profile.ts
+++ unigearrent-frontend/src/Models/Profile.ts
@@ -1,12 +1,14 @@
 import { RegistrationType } from "./RegistrationType";
 
 export class Profile{
+    Id: string;
     Username: string;
     PhoneNumber: string;
     Email: string;
     Token: string;
     Type: RegistrationType;
-    constructor(username: string, phoneNumber: string, email: string, token: string, type: RegistrationType){
+    constructor(id: string, username: string, phoneNumber: string, email: string, token: string, type: RegistrationType){
+        this.Id = id;
         this.Username = username;
         this.PhoneNumber = phoneNumber;
         this.Email = email;
