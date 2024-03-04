c5b043d308d943575164bc69597f9e3973fd7181
McGerasz <mcgerasz@gmail.com>
2024-03-04 13:11:27 +0100

    chore: update profile models with id property

--- unigearrent-frontend/src/Models/LessorProfile.ts
+++ unigearrent-frontend/src/Models/LessorProfile.ts
@@ -3,8 +3,8 @@ import { RegistrationType } from "./RegistrationType";
 
 export class LessorProfile extends Profile{
     Name: string;
-    constructor(username: string, phoneNumber: string, email: string, token: string, name: string){
-        super(username, phoneNumber, email, token, RegistrationType.Lessor)
+    constructor(id: string, username: string, phoneNumber: string, email: string, token: string, name: string){
+        super(id, username, phoneNumber, email, token, RegistrationType.Lessor)
         this.Name = name;
     }
 }
--- unigearrent-frontend/src/Models/UserProfile.ts
+++ unigearrent-frontend/src/Models/UserProfile.ts
@@ -4,8 +4,8 @@ import { RegistrationType } from "./RegistrationType";
 export class UserProfile extends Profile{
     FirstName: string;
     LastName: string;
-    constructor(username: string, phoneNumber: string, email: string, token: string, firstName: string, lastName: string){
-        super(username, phoneNumber, email, token, RegistrationType.User);
+    constructor(id: string,username: string, phoneNumber: string, email: string, token: string, firstName: string, lastName: string){
+        super(id, username, phoneNumber, email, token, RegistrationType.User);
         this.FirstName = firstName;
         this.LastName = lastName;
     }
