3a6ac1277a12ccf97faee87dc4069a84c12006f1
McGerasz <mcgerasz@gmail.com>
2024-03-06 10:57:47 +0100

    feat: rework the style of Post Cards

--- unigearrent-frontend/src/Components/PostCardsComponent.tsx
+++ unigearrent-frontend/src/Components/PostCardsComponent.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import { PostCardData } from "../Models/PostCardData";
 import { useNavigate } from "react-router-dom";
 
@@ -8,18 +8,18 @@ const PostCardsComponent: React.FC<{searchData: Array<PostCardData>, myPost: boo
         navigate(props.myPost ? `/mypost/${id}` : `/post/${id}`);
     }
     return(
-        <Row sm={1} md={2} lg={4}>{props.searchData.map((element: PostCardData) => {
-            console.log(element)
-            return <Col>
-                <Card onClick={() => ClickHandler(element.Id)} className="h-100">
+        <Container>{props.searchData.map((element: PostCardData) => {
+            return <Row className="justify-content-center">
+                <Card onClick={() => ClickHandler(element.Id)} className="h-100 mb-4 w-75" style={{border:"2px solid #d9b99b", cursor:"default"}}>
                 <Card.Body>
-                    <Card.Title style={{textAlign:"center"}}>{element.Name}</Card.Title>
+                    <Card.Title style={{textAlign:"center", marginBottom:"0px"}}>{element.Name}</Card.Title>
+                    <Container className="w-50"><hr className="mt-1 mb-2" style={{border:"1px solid black"}}></hr></Container>
                     <Card.Text  style={{textAlign:"center", lineHeight:"1.2em", height:"4.8em", overflow:"hidden", WebkitMaskImage:"linear-gradient(to bottom, black 50%, transparent 100%)", maskImage:"linear-gradient(to bottom, black 50%, transparent 100%)"}}>{element.Description}</Card.Text>
-                    <Card.Text  style={{textAlign:"center"}}>{element.Location}</Card.Text>    
+                    <Card.Text  style={{textAlign:"right"}}>{element.Location}</Card.Text>    
                 </Card.Body>       
             </Card>
-            </Col>
-        })}</Row>
+            </Row>
+        })}</Container>
     )
 }
 export default PostCardsComponent;
